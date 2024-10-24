2f452c09ea0fd44db111488ab95d59f2cd2ddc9f
maya <maryamallameh1991@gmail.com>
2024-10-24 11:13:05 +0200

    Latest changes made by person B

--- Main.java
+++ Main.java
@@ -3,10 +3,4 @@ public class Main {
   System.out.println("Hello from Drama Queen!");   // Ändring av Person A 
   } 
  }
-=======================================================
-public class Main {
- public static void main(String[] args) {
-  System.out.println("Hey Damir!");   // Ändring av Person B
-  }
- }
 
