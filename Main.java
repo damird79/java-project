d901ea4b58997358346fa6253df0ffc77760a5b6
maya <maryamallameh1991@gmail.com>
2024-10-24 11:07:08 +0200

    changes by person B

--- Main.java
+++ Main.java
@@ -3,4 +3,10 @@ public class Main {
   System.out.println("Hello from Drama Queen!");   // Ändring av Person A 
   } 
  }
+=======================================================
+public class Main {
+ public static void main(String[] args) {
+  System.out.println("Hey Damir!");   // Ändring av Person B
+  }
+ }
 
